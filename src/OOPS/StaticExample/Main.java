5c957365e205abbafd04f64ab128b537361ac5f5
DMad19 <dineshmadduru98813@gmail.com>
2024-08-02 08:35:06 +0530

    Add one more example of static and non static

--- src/OOPS/StaticExample/Main.java
+++ src/OOPS/StaticExample/Main.java
@@ -5,5 +5,15 @@ public class Main {
         Human Dhoni = new Human("Dhoni",44,222222,true);
         Human Kohli = new Human("Kohli",44,222222,true);
         System.out.println(Human.population);
+        message();
+    }
+    static void message(){
+        System.out.println("hi");
+        Main obj = new Main();
+        obj.message2(); //static. so obj required.
+    }
+    void message2(){
+//        message(); can call without a obj
+        System.out.println("hi2");
     }
 }
