206491029f559b839d1855524c688d172965a9b9
DMad19 <dineshmadduru98813@gmail.com>
2024-08-05 08:47:40 +0530

    Modify examples of Inheritance

--- src/OOPS/Properties/Inheritance/BoxWithWeight.java
+++ src/OOPS/Properties/Inheritance/BoxWithWeight.java
@@ -1,2 +1,18 @@
-package OOPS.Properties.Inheritance;public class BoxWithWeight {
+package OOPS.Properties.Inheritance;
+
+public class BoxWithWeight extends Box{
+    public int weight;
+    public BoxWithWeight(){
+        weight = -1;
+    }
+
+    public BoxWithWeight(int l, int w, int h, int weight) {
+        super(l, w, h);
+        this.weight = weight;
+    }
+
+    public BoxWithWeight(Box another, int weight) {
+        super(another);
+        this.weight = weight;
+    }
 }
--- src/OOPS/Properties/Inheritance/Main.java
+++ src/OOPS/Properties/Inheritance/Main.java
@@ -8,5 +8,11 @@ public class Main {
         System.out.println(box2.l+" "+box2.w+" "+box2.h);
         Box box3 = new Box(box2);
         System.out.println(box3.l+" "+box3.w+" "+box3.h);
+        BoxWithWeight box4 = new BoxWithWeight();
+        System.out.println(box4.l+" "+box4.w+" "+box4.h+" "+box4.weight);
+        BoxWithWeight box5 = new BoxWithWeight(1,2,3,4);
+        System.out.println(box5.l+" "+box5.w+" "+box5.h+" "+box5.weight);
+        BoxWithWeight box6 = new BoxWithWeight(box5,4);
+        System.out.println(box6.l+" "+box6.w+" "+box6.h+" "+box6.weight);
     }
 }
