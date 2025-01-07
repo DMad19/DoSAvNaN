39ee3191a5e24bf1e14dfd35de5ce1fce5a3e7e7
DMad19 <dineshmadduru98813@gmail.com>
2025-01-07 23:54:11 +0530

    added Python files - iteration/subsets

---
+++ pythonFiles/Iteration/Subsets/subsets.py
@@ -0,0 +1,22 @@
+def subsets(s):
+    l = [""]
+    for i in s:
+        for j in range(len(l)):
+            elem = l[j]+i
+            l.append(elem)
+    return l
+print(subsets("Abc"))
+
+def subsetsforDuplicates(s):
+    l = [""]
+    start = 0
+    end = 0
+    for i in range(len(s)):
+        start = 0
+        if i>0 and s[i]==s[i-1]:
+            start = end + 1
+        end = len(l)-1
+        for j in range(start,len(l)):
+            l.append(l[j]+s[i])
+    return l
+print(subsetsforDuplicates("Abcc")) 
