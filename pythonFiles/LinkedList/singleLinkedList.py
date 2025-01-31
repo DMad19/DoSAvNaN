39f94046ed8b3b7fc9e69a4bcb9ac5f3f39cd88f
DMad19 <dineshmadduru98813@gmail.com>
2025-01-31 23:21:29 +0530

    Add more methods to singleLL.py

--- pythonFiles/LinkedList/singleLinkedList.py
+++ pythonFiles/LinkedList/singleLinkedList.py
@@ -52,15 +52,59 @@ class SingleLinkedList:
             print(str(temp._data)+"->",end="")
             temp = temp._next
         print("None")
+    def deleteFirst(self):
+        if self._head==None:
+            return self._size
+        self._head = self._head._next
+        if(self._head==None):
+            self._tail = None
+        self._size -= 1
+        return self._size
+    def deleteLast(self):
+        if self._size == 0:
+            return self._size
+        if self._head==self._tail:
+            return self.deleteFirst()
+        # temp = self._head
+        # while(temp._next._next):
+        #     temp = temp._next
+        temp = self._get(self._size - 2)
+        temp._next = None
+        self._tail = temp
+        self._size -= 1
+        return self._size
+    def delete(self,index):
+        if self._size == 0:
+            return self._size
+        if index==0:
+            return self.deleteFirst()
+        if index==self._size-1:
+            return self.deleteLast()
+        node = self._get(index)
+        node._next = node._next._next
+        self._size -= 1
+        return self._size
+    def _get(self,index):
+        temp = self._head
+        for i in range(index):
+            temp = temp._next
+        return temp
+    def getNodeValue(self,value):
+        node = self._head
+        while(node):
+            if node._next == value:
+                return node
+            node = node._next
+        return None
 ll = SingleLinkedList()
 print(ll.insert(100,0))
-ll.displayLinkedList()
 print(ll.insertLast(4))
+print(ll.delete(1))
 ll.displayLinkedList()
-print(ll.insert(300,1))
+print(ll.insertFirst(5))
 ll.displayLinkedList()
-print(ll.insertFirst(3))
-print(ll.insert(200,3))
-print(ll.insert(200,0))
-print(ll.insert(200,1))
+print(ll.deleteLast())
 ll.displayLinkedList()
+print(ll.deleteFirst())
+ll.displayLinkedList()
+print(ll.delete(0))
