6e9b79e657282143f12790fb33f7ea77a8fa3828
DMad19 <dineshmadduru98813@gmail.com>
2025-02-21 07:57:10 +0530

    Add POTD 21-02-2025

---
+++ POTD/paranthesis_checker.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isBalanced(self, s):
+        # code here
+        stack = []
+        for i in s:
+            if i=='{' or i=='[' or i=='(':
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                if(matched(stack[-1],i)):
+                    stack.pop()
+                else:
+                    return False
+        return not stack
+
+def matched(top,i):
+    if (top=='[' and i==']') or (top=='(' and i==')') or (top=='{' and i=='}') :
+        return True
+    return False
